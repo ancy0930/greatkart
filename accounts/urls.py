cac714f8ba0d04273e292c575b334f8ec67a3a60
Ancy Ashraf <ancyashraf0930@gmail.com>
2024-03-11 11:13:03 +0530

    Commit Message

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("register/", views.register, name="register"),
+    path("login/", views.login, name="login"),  # Corrected name attribute
+    path("logout/", views.logout, name="logout"),
+]
--- greatkart/settings.py
+++ greatkart/settings.py
@@ -134,9 +134,15 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
 
 
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+    
+}
